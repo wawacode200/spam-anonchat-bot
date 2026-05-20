c90393a7f24caa85eb397a4a46c6ddb106d1581a
slaww <slaww@mail.com>
2026-05-20 18:03:30 +0500

    test

--- services/telethon_pool/sender.py
+++ services/telethon_pool/sender.py
@@ -87,7 +87,7 @@ class TelethonSender:
                             f"🎯 {session.name}: собеседник найден"
                         )
                         await asyncio.sleep(0.5)
-                        await client.send_message(entity=chat_id, message="👅 Хочешь грязный анонимный чат?")
+                        await client.send_message(entity=chat_id, message="👅 Это самый пошлый анонимный чат?")
                         await asyncio.sleep(0.1)
                         await client.send_message(entity=chat_id, message=text)
                         await asyncio.sleep(1)
